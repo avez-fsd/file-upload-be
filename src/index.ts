95d9edf3e765aa936bd8ef37ee1180b075d2d73a
avez-mohammad <avezfsd@gmail.com>
2025-06-22 14:19:39 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,13 @@ import routes from './routes'
 import addRequestId from '@middelwares/request-id.middleware';
 import response from "@helpers/response.helper"
 import express, { Express, NextFunction, Request, Response } from 'express';
+import logger from '@helpers/logger.helper';
 
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,4 +25,14 @@ app.use(response.handler5xx);
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+process.on('uncaughtException', (error) => {
+    logger.error(`Uncaught Exception`, error)
+    // Optionally, you can handle the error gracefully and close connections if needed
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Rejection`, reason);
+    // Optionally, you can handle the rejection gracefully and close connections if needed
 });
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import { upload } from '@middelwares/multer.middleware';
+import express, { Request, Response } from 'express';
+import response from '@helpers/response.helper';
 
 const router = express.Router({ mergeParams: true });
 
 
+router.post("/upload", upload.single('fileTest'), (req:Request,res:Response)=>{
+    console.log(req.file)
+    response.success(req, res);
+});
 
 export default router;
